a7f0c0e3e4b69e3f0cfdb260d3d83d7cc37470fa
Colin Campbell <colin.campbell@logicmonitor.com>
2023-05-03 18:02:34 -0700

    deps added with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <nav>
+        <ul>
+          <li>
+            <a href={`/recurring`}>Recurring</a>
+          </li>
+          <li>
+            <a href={`/cashflow`}>Cash Flow</a>
+          </li>
+        </ul>
+      </nav>
+    ),
+  },
+  {
+    path: "/recurring",
+    element: <div>Recurring!</div>,
+  },
+  {
+    path: "/cashflow",
+    element: <div>Cash flow!</div>,
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
